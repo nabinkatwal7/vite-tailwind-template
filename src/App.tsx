a33d6f1dbf74b7a86a8c1cd1766826f1188898cb
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2024-01-16 12:42:52 +0545

    template created

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl text-center text-red-700">
+        Welcome to Vite with TailwindCSS and React
+      </h1>
+    </div>
+  );
+}
+
+export default App;
