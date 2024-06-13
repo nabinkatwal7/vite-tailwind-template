3f7ed4f4f26f6020324cbb917aec680465a608b3
Nabin Katwal <nabinkatwal2000@gmail.com>
2024-06-13 12:58:20 +0545

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import "./App.css";
 
+import { AppRoute } from "../router/routes";
+
 function App() {
   return (
     <div>
-      <h1 className="text-3xl text-center text-red-700">
-        Welcome to Vite with TailwindCSS and React
-      </h1>
+      <AppRoute />
     </div>
   );
 }
